c09d23156277f116278fb4447188dc0365269c0e
ﷲﷴ﷼ﯼ <joaomotionless570@gmail.com>
2023-05-19 11:43:28 -0300

    Add files via upload

---
+++ python/Aula1.py
@@ -0,0 +1,18 @@
+#Declarando uma variavel chamada nome e dentro dela uma string
+nome = 'joao'  
+idade = 21 
+altura = 1.78
+
+#A função type eu declaro o tipo da variavel se ela é string, int, float etc.
+#E declaro o tipo do nome,idade e altura nesse modelo abaixo.
+tipo_nome = type (nome)
+tipo_idade = type (idade)
+tipo_altura = type (altura)
+
+#Aqui eu mando ele printar na tela o nome idade e o tipo delas 
+print(nome)
+print(idade)
+print(altura)
+print (tipo_nome)
+print (tipo_idade)
+print (tipo_altura)
