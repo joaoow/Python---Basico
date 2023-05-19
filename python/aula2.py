e8e622a329523b43f15624b49b04c53e7672e54d
ﷲﷴ﷼ﯼ <joaomotionless570@gmail.com>
2023-05-19 12:10:40 -0300

    Add files via upload

---
+++ python/aula2.py
@@ -0,0 +1,10 @@
+#A função input faz com que você possa escrever e digitar o que o código pede
+nome = input('Escreva seu nome: ') 
+idade = input('Digite sua idade: ') 
+altura = input('Agora digite sua altura: ')
+
+print (nome, 'tem', idade, 'anos', 'e', altura, 'de altura')
+print (type(nome), type(idade), type(altura))
+#Veja que ele printa as três variavel como "str", veremos esse problema mais a frente
+
+
